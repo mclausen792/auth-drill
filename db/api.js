41cec3145b47617b08ff670b819e1ef72e678ec3
Makayla Clausen <mclausen792@gmail.com>
2017-12-19 15:03:52 -0700

    Can create an account and log in

--- db/api.js
+++ db/api.js
@@ -1,8 +1,16 @@
 var knex = require('./knex')
 
 module.exports = {
-  signIn: function() {
+  signIn: function(body) {
+    console.log(body);
+return knex('my_user').where({
+  'agentName': body.agentName,
+}).first()
   },
-  signUp: function() {
+  signUp: function(body, hash) {
+      return knex('my_user').insert({
+        'agentName': body.agentName,
+        'password':hash
+      }).returning('*')
+    }
   }
-}
--- routes/users.js
+++ routes/users.js
@@ -3,31 +3,39 @@ var router = express.Router()
 var db = require('../db/api')
 var bcrypt = require('bcrypt')
 
+
 router.post('/signin', function(req, res, next){
-  db.signIn()
+  db.signIn(req.body)
   .then(function(agent){
-    //Use bcrypt to log in
+    bcrypt.compare(req.body.password, agent.password, function(err, isMatch){
       if (isMatch) {
-        //Route to /Assignment
+      res.render('index', {
+        title: 'gClassified',
+        message: 'Successfully logged in'
+        })
       }
       else {
         res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' })
       }
+    })
   })
 })
 
 router.post('/signup', function(req,res,next){
-  //Use bcrypt to Sign Up
 
-    db.signUp()
+  //Use bcrypt to Sign Up
+  bcrypt.hash(req.body.password, 10, function(err, hash){
+    db.signUp(req.body, hash)
     .then(function(agent){
       if (agent[0].password === req.body.password) {
         res.render('index', { title: 'gClassified', message: 'Password Must Be Hashed. Government Secrets are at Stake!' })
       }
       else {
         res.render('index', { title: 'gClassified', message: 'Sign Up Successful' })
-      }
+        }
+      })
     })
-})
+  })
+
 
 module.exports = router
